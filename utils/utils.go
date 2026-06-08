58bd470525fa67a4e4e97df2c03ea2ff472c4075
Ryan-Gosusluging <167106946+Ryan-Gosusluging@users.noreply.github.com>
2026-06-09 02:50:37 +0400

    Update utils.go

--- utils/utils.go
+++ utils/utils.go
@@ -1,10 +1,19 @@
 package utils
 
 import (
-    "math/rand"
-    "time"
+	"math/rand"
+	"sync"
+	"time"
 )
 
+var randMu, randSrc = &sync.Mutex{}, rand.NewSource(time.Now().UnixNano())
+
 func RandomDuration(min, max time.Duration) time.Duration {
-    return min + time.Duration(rand.Int63n(int64(max-min)))
+	randMu.Lock()
+	defer randMu.Unlock()
+	if min >= max {
+		return min
+	}
+	r := rand.New(randSrc)
+	return min + time.Duration(r.Int63n(int64(max-min)))
 }
